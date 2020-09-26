c16d94db5b0f423882d41cb0ba9c125c7d0764ba
Christopher Irvine <chris@chrisirvine.co.uk>
2020-09-26 23:18:04 +0100

    Introducted Spring Security with Github Oauth2 integration to webapp
    - Updated CakeWebAppIntegrationTest, setting mock properties for ClientRegistrationRepository bean construction, changed tests to support security model, added test to ensure unauthenticated users are redirected
    - Updated SpringConfiguration, added ClientRegistrationRepository with GitHub OAuth2 login support.  Client id and secret are passed in via environment properties for security.
    - Created SecurityConfiguration, supports CORS, disables CSRF as it'll block POST requests otherwise (don't do this in real life!), allows all requests to /cakes API and authenticates all other requests
    - Updated pom.xml, added spring-security-test necessary for setup of mock user in CakeWebAppIntegrationTest

--- src/main/java/com/waracle/cakemgr/configuration/SpringConfiguration.java
+++ src/main/java/com/waracle/cakemgr/configuration/SpringConfiguration.java
@@ -1,15 +1,25 @@
 package com.waracle.cakemgr.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 @Configuration
 public class SpringConfiguration {
+
+    private static final String GITHUB_CLIENT_ID = "github.client.id";
+    private static final String GITHUB_CLIENT_SECRET = "github.client.secret";
 
     @Value("${db.driver.class.name}")
     private String dbDriverClassName;
@@ -22,6 +32,13 @@ public class SpringConfiguration {
 
     @Value("${db.url}")
     private String dbUrl;
+
+    private final Environment environment;
+
+    @Autowired
+    public SpringConfiguration(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public DataSource dataSource() {
@@ -38,5 +55,15 @@ public class SpringConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration registration = CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId(environment.getProperty(GITHUB_CLIENT_ID))
+                .clientSecret(environment.getProperty(GITHUB_CLIENT_SECRET))
+                .build();
+
+        return new InMemoryClientRegistrationRepository(Arrays.asList(registration));
     }
 }
--- src/test/java/integration/CakesWebAppIntegrationTest.java
+++ src/test/java/integration/CakesWebAppIntegrationTest.java
@@ -1,15 +1,20 @@
 package integration;
 
 import com.waracle.cakemgr.CakeManagerApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -17,12 +22,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CakeManagerApplication.class)
-@AutoConfigureMockMvc
+@TestPropertySource(properties = {"github.client.id = foo;github.client.secret = bar"})
 public class CakesWebAppIntegrationTest {
 
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mockMvc;
 
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser("spring")
     @Test
     public void GIVEN_the_view_page_of_the_webapp_WHEN_retrieved_THEN_page_returned_okay() throws Exception {
         this.mockMvc.perform(get("/"))
@@ -31,6 +47,7 @@ public class CakesWebAppIntegrationTest {
                 .andExpect(status().isOk());
     }
 
+    @WithMockUser("spring")
     @Test
     public void GIVEN_the_add_page_of_the_webapp_WHEN_retrieved_THEN_page_returned_okay() throws Exception {
         this.mockMvc.perform(get("/add"))
@@ -39,6 +56,7 @@ public class CakesWebAppIntegrationTest {
                 .andExpect(status().isOk());
     }
 
+    @WithMockUser("spring")
     @Test
     public void GIVEN_the_add_post_endpoint_WHEN_invalid_data_supplied_THEN_validation_errors_returned() throws Exception {
         this.mockMvc.perform(post("/add"))
@@ -47,6 +65,7 @@ public class CakesWebAppIntegrationTest {
                 .andExpect(model().hasErrors());
     }
 
+    @WithMockUser("spring")
     @Test
     public void GIVEN_the_add_post_endpoint_WHEN_duplicate_title_supplied_THEN_validation_error_returned() throws Exception {
         this.mockMvc.perform(post("/add")
@@ -63,5 +82,12 @@ public class CakesWebAppIntegrationTest {
                 .andDo(print())
                 .andExpect(model().hasErrors())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void GIVEN_an_unauthenticated_user_WHEN_attempting_to_access_web_application_THEN_redirected_to_login() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
     }
 }
